865f742e97a73f47871ad31828a95b3ccd127c59
rtrhee56-hasht <rtrhee56@gmail.com>
2026-07-09 14:30:42 +0300

    Set home page to survey

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SurveyController;
 
+Route::get('/', [SurveyController::class, 'show']);
+
 Route::get('/survey', [SurveyController::class, 'show']);
 Route::post('/survey', [SurveyController::class, 'submit'])->name('survey.submit');
